5286bdc0db944d1f376390d81dd2abcf8b597056
Wudi Xie <wuxie@ucsd.edu>
2020-06-10 09:22:37 -0700

    CCM_test

--- pywrds/crspcomp.py
+++ pywrds/crspcomp.py
@@ -38,7 +38,6 @@ Provide merging and processing function for CRSP / Compustat
 import pandas as pd
 import datetime as dt
 
-class ccm():
 
     def __init__(self, file_crsp, file_comp, link_table, crsp_freq=None, comp_freq=None, linkprim=None, linktype=None, start_date=None, end_date=None):
         
@@ -78,9 +77,8 @@ class ccm():
 
 
 
-    def merge_crsp_compustat(file_crsp, file_comp, link_table, columns, key=['gvkey', 'date', 'datadate']):
+    def merge_crsp_compustat(file_crsp, file_comp, link_table, columns=None, key=['gvkey', 'date', 'datadate']):
         
-        # Solution 1: let end user input the frequency of imported data
         # Solution 2: check the frequency for imported data with code
 
         # Add the necessary columns to process the data (permno, permco, link type, link primary)
@@ -98,7 +96,7 @@ class ccm():
         df_crsp['day'] = pd.DatetimeIndex(df_crsp['date']).day # only for daily data!!!
 
         # Filter the linking table data
-        df_lt = df_lt[(df_lt.linktype == 'LC' or 'LU') & df_lt[(df_lt.linkprim == 'P' or 'C')
+        df_lt = df_lt[(df_lt.linktype == 'LC' or 'LU') & df_lt[(df_lt.linkprim == 'P' or 'C')]
 
         # Add GVKEY to CRSP monthly data using the linking table
         df_lt_crsp = pd.merge(df_lt, df_crsp, left_on=['permco', 'permno'], right_on=['lpermco', 'lpermno'])
@@ -110,8 +108,6 @@ class ccm():
         # df_crsp = pd.merge(df_crsp, crsp_processed_ret, on=['permno','date']
         # df_crsp = pd.merge(df_crsp, crsp_processed_stats_dsf, on=['permno','date']
         
-        # Drop useless daily statistics (too specific!!)
-        # df_crsp.drop(columns = ['ret', 'vol', 'ask', 'bid', 'askhi', 'bidlo'])
 
         # Merge CRSP data with GVKEY with Compustat data
         df_ccm = pd.merge(df_comp, df_lt_crsp, how=right, left_on=['gvkey', 'datadate']), right_on=['gvkey', 'date'])
