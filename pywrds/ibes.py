3e354c976d5caa70a0a11d1ed30b82bca9315248
Wudi Xie <wuxie@ucsd.edu>
2020-06-11 11:05:59 -0700

    Add_return_data

--- pywrds/ibes.py
+++ pywrds/ibes.py
@@ -27,3 +27,16 @@ def consensus(w, file_det, file_adjfactors=None, file_adj=None):
     elif file_adj is None:
         raise Exception("Please provide either the adjustment factor file",
                         " or the adjustment file from IBES.")
+
+    def add_ret2anndates(w, file_original, file_crsp_daily, anndates, x_days)
+
+        # Need columns GVKEY, datetime, anndates
+
+        # Check if everything is alright(e.g. no return data in original dataset), otherwise raise exception
+
+
+
+        # Add return data to original dataset on "permno"
+        df = pd.merge(file_original, file_crsp_daily["ret"], how='left',on='permno')
+
+        return df
